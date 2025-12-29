4a0f063df7346736c9688fa10119547220ebc921
Ankit Shakya <147362007+laksoo11@users.noreply.github.com>
2025-12-29 13:02:24 +0545

    Create and reuse components

---
+++ GameStore.Frontend/Clients/GamesClient.cs
@@ -0,0 +1,63 @@
+using System;
+using GameStore.Frontend.Models;
+
+namespace GameStore.Frontend.Clients;
+
+public class GamesClient
+{
+
+    private readonly List <GameSummary> games = 
+
+    [
+    new (){
+        Id = 1,
+        Name = "Witcher 3",
+        Genre = "Fantasy RPG",
+        Price = 15.99M,
+        ReleaseDate = new DateOnly(2015,5,20)
+        
+    },
+
+     new (){
+        Id = 2,
+        Name = "Cyberpunk 2077",
+        Genre = "Future RPG",
+        Price = 20.99M,
+        ReleaseDate = new DateOnly(2020,9,20)},
+
+     new (){
+        Id = 3,
+        Name = "Baldur's Gate 3",
+        Genre = "Fantasy RPG",
+        Price = 15.99M,
+        ReleaseDate = new DateOnly(2023,3,20)},
+
+    new (){
+        Id = 4,
+        Name = "GTA V",
+        Genre = "RPG",
+        Price = 5.99M,
+        ReleaseDate = new DateOnly(2014,6,20)},
+
+    new (){
+        Id = 5,
+        Name = "God Of War",
+        Genre = "RPG",
+        Price = 5.99M,
+        ReleaseDate = new DateOnly(2018,3,22)},
+
+     new (){
+        Id = 6,
+        Name = "Elden Ring",
+        Genre = "RPG, Boss Fight, Fighiting",
+        Price = 5.99M,
+        ReleaseDate = new DateOnly(2022,11,22)}, 
+
+    
+
+    ];
+
+    public GameSummary[] GetGames() => [.. games];
+
+
+}
--- GameStore.Frontend/obj/Debug/net10.0/GameStore.Frontend.AssemblyInfo.cs
+++ GameStore.Frontend/obj/Debug/net10.0/GameStore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30decf4cf99329f33c90594219e2e066766eca5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
