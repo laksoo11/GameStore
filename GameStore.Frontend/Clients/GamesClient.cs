5a6f6556449eaf920f29079de0ef798d85d29c14
Ankit Shakya <147362007+laksoo11@users.noreply.github.com>
2025-12-29 19:22:38 +0545

    Use Forms and Bindings

--- GameStore.Frontend/Clients/GamesClient.cs
+++ GameStore.Frontend/Clients/GamesClient.cs
@@ -53,11 +53,32 @@ public class GamesClient
         Price = 5.99M,
         ReleaseDate = new DateOnly(2022,11,22)}, 
 
-    
-
     ];
 
+    private readonly Genre[] genres = new GenresClient().GetGenres();
+
     public GameSummary[] GetGames() => [.. games];
+
+    public void AddGame(GameDetails game)
+
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(game.GenreId);
+
+        var genre = genres.Single(genre => genre.Id == int.Parse(game.GenreId));
+
+        var gameSummary = new GameSummary
+        {
+            Id = games.Count + 1,
+            Name = game.Name,
+            Genre = genre.Name,
+            Price = game.Price,
+            ReleaseDate = game.ReleaseDate,
+
+        };
+
+        games.Add(gameSummary);
+    }
+
 
 
 }
---
+++ GameStore.Frontend/Clients/GenresClient.cs
@@ -0,0 +1,38 @@
+using System;
+using GameStore.Frontend.Models;
+
+namespace GameStore.Frontend.Clients;
+
+public class GenresClient
+{
+    private readonly Genre[] genres =
+    [
+        new (){
+            Id = 1,
+            Name = "Fighting"
+        },
+
+         new (){
+            Id = 2,
+            Name = "RPG"
+        },
+
+         new (){
+            Id = 3,
+            Name = "Sports"
+        },
+
+         new (){
+            Id = 4,
+            Name = "Racing"
+        },
+
+         new (){
+            Id = 5,
+            Name = "Fantasy"
+        },
+    ];
+
+    public Genre[] GetGenres() => genres;
+
+}
