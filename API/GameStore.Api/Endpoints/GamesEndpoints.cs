08eb9f37653cd5bfb113048dd216b0c232b69989
Ankit Shakya <147362007+laksoo11@users.noreply.github.com>
2026-01-01 17:38:47 +0545

    using extension methods

---
+++ API/GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -0,0 +1,106 @@
+using System;
+using GameStore.Api.Dtos;
+
+namespace GameStore.Api.Endpoints;
+
+public static class GamesEndpoints
+{
+const string GetGameEndpointName = "GetGame";
+
+
+private static readonly List<GameDto> games = [
+    new (
+        1,
+        "Witcher 3",
+        "RPG",
+        15.99M,
+        new DateOnly(2015,5,20)),
+
+    new (
+        2,
+        "Cyberpunk 2077",
+        "RPG",
+        20.99M,
+        new DateOnly(2015,5,20)),
+
+    new (
+        3,
+        "Baldur's Gate 3",
+        "RPG",
+        15.99M,
+        new DateOnly(2023,3,20)),
+
+     new (
+        4,
+        "Gta V",
+        "RPG",
+        5.99M,
+        new DateOnly(2014,6,20)),
+    ];
+
+    public static WebApplication MapGamesEndpoints(this WebApplication app)
+    {
+
+// GET /games
+app.MapGet("games", () => games);
+
+//GET /games/1
+
+app.MapGet("games/{id}", (int id) => 
+{
+    GameDto? game = games.Find(game => game.Id ==id);
+
+    return game is null ? Results.NotFound() : Results.Ok(game);
+})
+    .WithName("GetGameEndpointName");
+
+// POST /games
+app.MapPost("games", (CreateGameDto newGame) =>
+{
+    GameDto game = new(
+        games.Count +1,
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate);
+    
+    games.Add(game);
+    return Results.CreatedAtRoute("GetGameEndpointName", new {id = game.Id}, game);
+});
+
+// PUT /games
+app.MapPut("games/{id}", (int id, UpdateGameDto updateGame) =>
+{   
+    var index = games.FindIndex(game => game.Id == id);
+
+    if (index == -1)
+    {
+        return Results.NotFound();
+    }
+
+    games[index] = new GameDto(
+        id,
+        updateGame.Name,
+        updateGame.Genre,
+        updateGame.Price,
+        updateGame.ReleaseDate
+    );
+    return Results.NoContent(); 
+});
+
+//DELETE /games/1
+app.MapDelete("games/{id}", (int id) =>
+{
+    games.RemoveAll(game => game.Id ==  id);
+
+    return Results.NoContent();
+    
+});
+
+return app;
+
+}
+
+
+
+}
--- API/GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ API/GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2010c6f8bbd49d6ab3a3a517feb6a6e0276bfd1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b74ad0dad4330a74b3fa584612acce0b4c4b870")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
